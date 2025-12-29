82b96ad4f9f3af4db3855ed9643f249289c5cb61
Diyar Niyazov <diyar.niyazov08@gmail.com>
2025-12-28 18:59:58 -0600

    Day 4: Printing Department

--- Day4.java
+++ Day4.java
@@ -1,4 +1,4 @@
-// --- Day 4: Printing Department ---
+// --- Day 4: Printing Department --- 
 // You ride the escalator down to the printing department. They're clearly getting ready for Christmas; they have lots of large rolls of paper everywhere, and there's even a massive printer in the corner (to handle the really big print jobs).
 
 // Decorating here will be easy: they can make their own decorations. What you really need is a way to get further into the North Pole base while the elevators are offline.
@@ -120,3 +120,4 @@ public class Day4 {
         System.out.println(accessibleRolls);
     }
 }
+
